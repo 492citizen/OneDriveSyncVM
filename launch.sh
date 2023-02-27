4697fd93473bb133ed215b6a184ed5db2aa73d92
492citizen <492citizen@protonmail.com>
2023-02-27 19:45:13 +0100

    Added .mount and .automount systemd files. Edited README.md. Added launch11.sh to launch a Windows 11 machine.

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 # Run VM normally (with display, not headless)
-qemu-system-x86_64 -enable-kvm -drive file=<Path-to-Image.img> -m 2G -cpu host -smp 1 -vga virtio -display sdl,gl=on -netdev bridge,br=virbr0,id=net0,helper=/usr/lib/qemu/qemu-bridge-helper -device e1000,netdev=net0
+qemu-system-x86_64 -enable-kvm -drive file=<Path-to-Image.img> -m 2G -cpu host -smp 1 -vga virtio -display sdl,gl=on -netdev bridge,br=virbr0,id=net0 -device e1000,netdev=net0
